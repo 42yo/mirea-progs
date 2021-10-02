0eadde3bf88471b7e2239d76aaeaedda5393b0e2
42yo <kqlys@yandex.ru>
2021-10-02 11:31:38 +0300

    10

---
+++ Task_10/Task_10.jl
@@ -0,0 +1,43 @@
+function AverageTemperature(r)
+    sum = 0
+    counter = 0
+    side = Ost
+    while !isborder(r, Nord)
+        sum, counter = movementsandcheckformarker(r, side, sum, counter)
+        sum, counter = checktemperature(r, sum, counter)
+        move!(r, Nord)
+        side = inverse(side)
+    end
+    sum, counter = movementsandcheckformarker(r, side, sum, counter)
+    sum, counter = checktemperature(r, sum, counter)
+    return sum/counter
+end
+
+function movementsandcheckformarker(r, side, s, c)
+    while !isborder(r, side)
+        if ismarker(r)
+            s += temperature(r)
+            c += 1 
+        end
+        move!(r, side)
+    end
+    return s, c
+end
+
+function movements(r, side)
+    while !isborder(r, side) 
+        move!(r, side)
+    end
+end
+
+function inverse(side)
+    return HorizonSide((Int(side) + 2) % 4)
+end
+
+function checktemperature(r, s, c)
+    if ismarker(r)
+        s += temperature(r) 
+        c += 1
+    end
+    return s, c
+end
