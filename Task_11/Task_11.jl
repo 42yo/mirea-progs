45c7bc47e6d919219f6709d714319c8a27207604
42yo <kqlys@yandex.ru>
2021-10-04 00:16:26 +0300

    13

---
+++ Task_11/Task_11.jl
@@ -0,0 +1,26 @@
+function XTypeKrest(r)
+    for i in 0:3
+        GoDiagonalAndMark(r, HorizonSide(i))
+    end
+end
+
+function GoDiagonalAndMark(r, side)
+    sides = (side, HorizonSide((Int(side) + 1) % 4))
+    counter = 0
+    while !isborder(r, sides[1]) && !isborder(r, sides[2])
+        putmarker!(r)
+        move!(r, sides[1])
+        move!(r, sides[2])
+        putmarker!(r)
+        counter += 1
+    end
+    while counter != 0
+        move!(r, inverse(sides[2]))
+        move!(r, inverse(sides[1]))
+        counter -= 1
+    end
+end
+
+function inverse(side)
+    return HorizonSide((Int(side) + 2) % 4)
+end 
